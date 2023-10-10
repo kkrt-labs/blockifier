0c0003347a2fcba1821cec741de88c61a64cf79a
Harsh Bajpai <harshbajpai@Harshs-MacBook-Pro.local>
2023-10-10 09:58:21 +0530

    dev: remove not required tests

--- crates/blockifier/src/execution/contract_class.rs
+++ crates/blockifier/src/execution/contract_class.rs
@@ -397,38 +397,9 @@ fn convert_entry_points_v1(
 
 #[cfg(test)]
 mod test {
-    use std::{fs, sync::Arc};
+    use std::fs;
 
-    use crate::execution::contract_class::{ContractClass, ContractClassV0, ContractClassV1Inner};
-
-    use super::ContractClassV0Inner;
-
-    #[test]
-    fn test_serialization_inner_class_v0() {
-        let class_inner = ContractClassV0Inner::default();
-        let class_inner = Arc::new(class_inner);
-        let class_inner = ContractClassV0(class_inner);
-        let value = serde_json::to_string_pretty(&class_inner).unwrap();
-        println!("value is -----> {}", value)
-    }
-
-    #[test]
-    fn test_serialization_contract_class() {
-        let class_inner = ContractClassV0Inner::default();
-        let class_inner = Arc::new(class_inner);
-        let class_inner = ContractClassV0(class_inner);
-
-        let contract_class = ContractClass::V0(class_inner);
-        let value = serde_json::to_string_pretty(&contract_class).unwrap();
-        println!("value is -----> {}", value)
-    }
-
-    #[test]
-    fn test_serialization_inner_class_v1() {
-        let class_inner = ContractClassV1Inner::default();
-        let value = serde_json::to_string_pretty(&class_inner).unwrap();
-        println!("value is -----> {}", value)
-    }
+    use crate::execution::contract_class::ContractClassV0;
 
     #[test]
     fn test_deserialization_of_contract_class_v_0() {
